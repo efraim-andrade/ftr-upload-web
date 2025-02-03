eb5750b0930de34a0c68fd81a6971217bff00743
Efraim Andrade <efraim.dev@gmail.com>
2025-02-03 09:29:51 -0300

    add the component list and item styles

--- src/components/upload-widget/upload-list.tsx
+++ src/components/upload-widget/upload-list.tsx
@@ -1,3 +1,16 @@
+import { UploadWidgetUploadItem } from './upload-item'
+
 export function UploadWidgetList() {
-  return <div className="px-3 h-48" />
+  return (
+    <div className="flex-col gap-3 px-4">
+      <span className="text-xs font-medium ">
+        Uploaded files <span className="text-zinc-400">(2)</span>
+      </span>
+
+      <div className="flex flex-col gap-2 mt-2">
+        <UploadWidgetUploadItem />
+        <UploadWidgetUploadItem />
+      </div>
+    </div>
+  )
 }
