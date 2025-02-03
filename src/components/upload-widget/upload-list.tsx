1b64489bd41b2c779fc624d8db567b299f77621e
Efraim Andrade <efraim.dev@gmail.com>
2025-02-03 09:40:16 -0300

    add static state

--- src/components/upload-widget/upload-list.tsx
+++ src/components/upload-widget/upload-list.tsx
@@ -1,16 +1,22 @@
 import { UploadWidgetUploadItem } from './upload-item'
 
 export function UploadWidgetList() {
+  const isUploadListEmpty = false
+
   return (
     <div className="flex-col gap-3 px-4">
       <span className="text-xs font-medium ">
         Uploaded files <span className="text-zinc-400">(2)</span>
       </span>
 
-      <div className="flex flex-col gap-2 mt-2">
-        <UploadWidgetUploadItem />
-        <UploadWidgetUploadItem />
-      </div>
+      {isUploadListEmpty ? (
+        <span className="text-zinc-400 text-xs">No uploads added</span>
+      ) : (
+        <div className="flex flex-col gap-2 mt-2">
+          <UploadWidgetUploadItem />
+          <UploadWidgetUploadItem />
+        </div>
+      )}
     </div>
   )
 }
